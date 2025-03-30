0b0d851a1dffc280a9ae1b1f1f2f3956a56f16b4
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2025-03-30 22:58:22 +0700

    Update settings.py

--- jobapp/jobapp/settings.py
+++ jobapp/jobapp/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'oauth2_provider',
     'django_filters',
     "corsheaders",
-    "send_email.apps.SendEmailConfig",
 ]
 
 MIDDLEWARE = [
@@ -195,12 +194,3 @@ CLIENT_SECRET = 'dlGToUtM2aP2Jz9EnE5caCeAMShj2HN3ScEeTnIfdnXJPIFL7xwIcd0Ky1ozCTJ
 #     "password": "1",
 #     "grant_type": "password"
 # }
-
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"  # Hoặc SMTP của dịch vụ bạn dùng
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "nguyendeptraiiris@gmail.com"
-EMAIL_HOST_PASSWORD = "cwnh vvkf muis ijir"
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
