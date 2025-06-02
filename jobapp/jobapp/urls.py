8eee8ffcfd167f25e048279c08a3addd2c968789
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2025-06-02 17:42:29 +0700

    Fix get url image của Follow và Review nhà tuyển dụng, tắt token firebase

--- jobapp/jobapp/urls.py
+++ jobapp/jobapp/urls.py
@@ -21,7 +21,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from jobs.firebase_views import FirebaseTokenView
+# from jobs.firebase_views import FirebaseTokenView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -45,6 +45,6 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),name = 'schema-swagger-ui'),
     re_path(r'^redoc/$',schema_view.with_ui('redoc', cache_timeout=0),name='schema-redoc'),
     path('o/', include('oauth2_provider.urls',namespace='oauth2_provider')),
-    path('firebase-token/', FirebaseTokenView.as_view(), name='firebase-token'),
+    # path('firebase-token/', FirebaseTokenView.as_view(), name='firebase-token'),
 ]
 
--- jobapp/jobs/firebase_views.py
+++ jobapp/jobs/firebase_views.py
@@ -1,13 +1,13 @@
-from firebase_admin import auth as firebase_auth
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-
-class FirebaseTokenView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        user = request.user
-        firebase_uid = f"user_{user.id}"  # Đặt UID Firebase duy nhất
-        token = firebase_auth.create_custom_token(firebase_uid)
-        return Response({"firebase_token": token.decode('utf-8')})
+# from firebase_admin import auth as firebase_auth
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework.permissions import IsAuthenticated
+#
+# class FirebaseTokenView(APIView):
+#     permission_classes = [IsAuthenticated]
+#
+#     def get(self, request):
+#         user = request.user
+#         firebase_uid = f"user_{user.id}"  # Đặt UID Firebase duy nhất
+#         token = firebase_auth.create_custom_token(firebase_uid)
+#         return Response({"firebase_token": token.decode('utf-8')})
